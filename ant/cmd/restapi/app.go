e32ef9a2a60b2ba6341234905ecad08e03a9bf8f
kd.shi <shikd@bjzdgt.com>
2019-11-06 16:02:06 +0800

    restruct project

---
+++ ant/cmd/restapi/app.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+)
+
+type User struct {
+	Id, Name string
+}
+
+type UserResource struct {
+	users map[string]User
+}
+
+func (u UserResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.
+		Path("/users").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.GET("/{user-id}").To(u.findUser))
+	ws.Route(ws.POST("").To(u.updateUser))
+	ws.Route(ws.PUT("/{user-id}").To(u.createUser))
+	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser))
+
+	container.Add(ws)
+}
+
+func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("user-id")
+	user, ok := u.users[id]
+	if !ok {
+		response.AddHeader("Content-Type", "test/plain")
+		response.WriteErrorString(http.StatusNotFound, "User could not be found")
+	} else {
+		response.WriteEntity(user)
+	}
+}
+
+func (u UserResource) updateUser(request *restful.Request, response *restful.Response) {
+	usr := new(User)
+	err := request.ReadEntity(&usr)
+	if err == nil {
+		u.users[usr.Id] = *usr
+		response.WriteEntity(usr)
+	} else {
+		log.Printf(err.Error())
+		response.AddHeader("Content-Type", "test/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
+}
+
+func (u UserResource) createUser(request *restful.Request, response *restful.Response) {
+	usr := User{Id: request.PathParameter("user-id")}
+	err := request.ReadEntity(&usr)
+	if err == nil {
+		u.users[usr.Id] = usr
+		response.WriteHeaderAndEntity(http.StatusCreated, usr)
+	} else {
+		response.AddHeader("Content-Type", "test/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
+
+}
+
+func (u UserResource) removeUser(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("user-id")
+	delete(u.users, id)
+}
+
+func main() {
+	wsContainer := restful.NewContainer()
+	wsContainer.Router(restful.CurlyRouter{})
+	u := UserResource{map[string]User{}}
+	u.Register(wsContainer)
+
+	log.Printf("start listening on localhost:8080")
+
+	server := &http.Server{Addr: ":8080", Handler: wsContainer}
+
+	log.Fatal(server.ListenAndServe())
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/skdong/ant
 
 go 1.13
 
-require github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+	github.com/emicklei/go-restful v2.11.1+incompatible
+)
